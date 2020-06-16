b9e616293be8742ebe3efc0401ceb91533f11a79
YUKIKO IKEDA <applelele@gmail.com>
2020-06-16 03:24:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,9 +25,9 @@ def won?(board)
     position_2 = board[win_index_2]
     position_3 = board[win_index_3]
 
-    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return win_combination # return the win_combination indexes that won.
-    elsif #check if the board is empty
+    else
       false
     end
   end
